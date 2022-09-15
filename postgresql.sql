e297c661f17cccf6f1809c81ba56c7bcba9f703e
wlunaj111 <58739856+wlunaj111@users.noreply.github.com>
2022-09-15 15:23:51 -0400

    .
    .

--- postgresql.sql
+++ postgresql.sql
@@ -1,4 +1,4 @@
 select * from app_comentario
-WHERE app_comentario.texto ilike any(array['%palabraclave%'])
+WHERE app_comentario.texto ilike any(array['%palabraclave%', '%palabraclave2%'])
 
 limit 100
